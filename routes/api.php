327ef712a1d64d1a629749cd69ea69209fdc1e48
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2023-08-17 17:56:30 +0600

    Adding user-login API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+Route::prefix('v1/user')->group(function () {
+
+    Route::post('/login', [AuthController::class, 'login']);
+
+});
+
+
