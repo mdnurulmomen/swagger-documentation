0314b9bea7e8cedfad95da17f45b25e821672193
Md Nurul Momen <mdnurulmomen.bd@gmail.com>
2023-08-21 15:43:22 +0600

    Adding middleware to protect admin-routes

---
+++ app/Http/Middleware/UserIsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserIsAdmin
+{
+    use ApiResponser;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user() &&  Auth::user()->is_admin == 1) {
+            return $next($request);
+        }
+
+        return $this->generalApiResponse(403, [], 'You don not have permission to access this route');
+    }
+}
