112791680c087997c80eab7a0977e67d5c9fd533
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2023-08-24 16:55:24 +0600

    Generating API documentation for promotions.index route

--- app/Http/Controllers/Api/V1/PromotionController.php
+++ app/Http/Controllers/Api/V1/PromotionController.php
@@ -10,7 +10,66 @@ use App\Http\Resources\Api\V1\PromotionCollection;
 class PromotionController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     *
+     * @OA\Get(
+     *     path="/api/v1/main/promotions",
+     *     tags={"Main-Page"},
+     *     summary="List all promotions",
+     *     operationId="getPromotionList",
+     *     @OA\Parameter(
+     *         name="page",
+     *         in="query",
+     *         description="Page Number of Pagination",
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="limit",
+     *         in="query",
+     *         description="Number of elements at per page when paginating",
+     *         @OA\Schema(
+     *             type="integer",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sortBy",
+     *         in="query",
+     *         description="Name of the field for sorting",
+     *         @OA\Schema(
+     *             type="string",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="desc",
+     *         in="query",
+     *         description="Expected order of data to search users",
+     *         @OA\Schema(
+     *             type="boolean",
+     *             enum={true, false},
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server error"
+     *     )
+     * )
      */
     public function getPromotionList(Request $request)
     {
