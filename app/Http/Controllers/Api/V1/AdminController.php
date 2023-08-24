82effbe4cb117f1da3405fe3e57f78919c47d7c2
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2023-08-24 16:21:19 +0600

    Separating operationId for generating API documentation of admin.store route

--- app/Http/Controllers/Api/V1/AdminController.php
+++ app/Http/Controllers/Api/V1/AdminController.php
@@ -183,6 +183,116 @@ class AdminController extends Controller
     }
 
     /**
+     * @OA\Post(
+     *      path="/api/v1/admin/create",
+     *      tags={"Admin"},
+     *      summary="Create an Admin account",
+     *      operationId="storeUser",
+     *      security={{"bearerAuth":{}}},
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Request properties",
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 required={"first_name","last_name", "email", "password", "password_confirmation", "address", "phone_number"},
+     *                 @OA\Property(
+     *                     property="first_name",
+     *                     description="User firstname",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="last_name",
+     *                     description="User lastname",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     description="User email",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     description="User password",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password_confirmation",
+     *                     description="User password",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="address",
+     *                     description="User main address",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="phone_number",
+     *                     description="User main phone number",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="is_marketing",
+     *                     description="User marketing preference",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *         response=200,
+     *         description="Successful operation"
+     *      ),
+     *      @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *      ),
+     *      @OA\Response(
+     *         response=404,
+     *         description="Page not found"
+     *      ),
+     *      @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *         response=500,
+     *         description="Internal Server error"
+     *      )
+     *  )
+     */
+    public function storeUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|min:3|max:255',
+            'last_name' => 'required|string|min:3|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|max:255|confirmed',
+            'avatar' => 'nullable|string|max:255',
+            'address' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:255',
+            'is_marketing' => 'nullable|boolean'
+        ]);
+
+        if($validator->fails()){
+            return $this->generalApiResponse(422, [], null, $validator->messages());
+        }
+
+        $inputedDataArray = $validator->validated();
+
+
+        if ($this->guard()->user()->is_admin) {
+
+            $inputedDataArray += ['is_admin' => 1];
+
+        }
+
+        $newUser = User::create($inputedDataArray);
+
+        return $this->generalApiResponse(200, [$newUser]);
+    }
+
+    /**
      * Update an existing user.
      *
      * @OA\Put(
