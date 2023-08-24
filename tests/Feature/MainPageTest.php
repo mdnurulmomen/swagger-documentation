fdaf39cd45423a5fb7beb9bc1a5e03660569e3dd
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2023-08-24 23:13:38 +0600

    TestCase for main.blogs.show route

--- tests/Feature/MainPageTest.php
+++ tests/Feature/MainPageTest.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Feature;
 
+use Tests\TestCase;
 use App\Models\Post;
 use App\Models\Promotion;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 
 class MainPageTest extends TestCase
 {
@@ -27,10 +28,40 @@ class MainPageTest extends TestCase
      */
     public function test_post_list_method_returns_proper_response(): void
     {
-        $categories = Post::factory()->count(3)->create();
+        Post::factory()->count(3)->create();
 
         $response = $this->getJson(route('main.blogs.index'));
 
         $response->assertJsonCount(3, 'data');
+    }
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_post_show_method_returns_proper_response(): void
+    {
+        Post::truncate();
+
+        $title = fake()->title();
+
+        $newPost = Post::create([
+            'title' => $title,
+            'slug' => str_replace(' ', '-', $title),
+            'content' => Str::random(10),
+            'metadata' => json_encode([
+                fake()->randomElement([
+                    "author"=> "string",
+                    "image"=> "UUID from petshop.files"
+                ])
+            ]),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        $response = $this->getJson(route('main.blogs.show', ['uuid' => $newPost->uuid]));
+
+        $response->assertStatus(200)
+        ->assertJsonCount(1, 'data')
+        ->assertJsonPath('data.id', $newPost->id);
     }
 }
