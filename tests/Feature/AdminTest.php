fcee8dafd97c8bc44ac30dd398e4f670b2220438
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2023-08-24 23:33:27 +0600

    TestCase for admin.users.store route

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -27,4 +27,40 @@ class AdminTest extends TestCase
         ->assertJsonPath('success', true)
         ->assertJsonCount(1, 'data');
     }
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_store_method_returns_successfull_response(): void
+    {
+        $admin = User::factory()->create([
+            'password' => Hash::make('password'),
+            'is_admin' => 1,
+        ]);
+
+        $token = JWTAuth::attempt(['email'=> $admin->email, 'password'=> 'password', 'is_admin'=> 1]);
+
+        $payload = [
+            'uuid' => Str::uuid(),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'is_admin' => rand(0, 1),
+            'is_marketing' => rand(0, 1),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'address' => fake()->address(),
+            'phone_number' => fake()->unique()->e164PhoneNumber(),
+            'remember_token' => Str::random(10),
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => "Bearer $token",
+        ])->postJson(route('admin.users.store', $payload));
+
+        $response->assertStatus(200)
+        ->assertJsonCount(1, 'data')
+        ->assertJsonPath('success', true);
+    }
 }
