60c7959f35cc47e090a9b16cb79e3437e849347d
mdnurulmomen <mdnurulmomen.bd@gmail.com>
2023-08-25 12:52:22 +0600

    Adding authorization with swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,6 +18,15 @@ use Illuminate\Routing\Controller as BaseController;
  * @OA\Server(
  *     url="http://127.0.0.1:8000/",
  * )
+ * @OA\SecurityScheme(
+ *    securityScheme="bearerAuth",
+ *    type="http",
+ *    scheme="bearer",
+ *    description="Authorization token obtained from logging in",
+ *    name="Authorization",
+ *    in="header",
+ *    bearerFormat="JWT",
+ * )
  */
 class Controller extends BaseController
 {
